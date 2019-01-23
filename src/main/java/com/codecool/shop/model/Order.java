861cac22b7d8745c8d973b61fb6b28032bc3c614
Aron Gida <ifAronisGida@github.com>
2019-01-23 14:30:07 +0100

    added Order class

---
+++ src/main/java/com/codecool/shop/model/Order.java
@@ -0,0 +1,21 @@
+package com.codecool.shop.model;
+
+import java.util.ArrayList;
+
+public class Order extends BaseModel {
+
+    private ArrayList<Product> orderedItems;
+    private ArrayList<String> orderDetails;
+    private float totalPrice;
+    private int orderId;
+    private static int counter = 0;
+    private boolean paid;
+
+    public Order(String name, ArrayList<String> orderDetails, float totalPrice) {
+        super(name);
+        this.orderDetails = orderDetails;
+        this.totalPrice = totalPrice;
+        this.id = ++counter;
+        this.paid = false;
+    }
+}
