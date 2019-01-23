2422b08e7d98ab2b1505e9d0f672636c43927847
Aron Gida <ifAronisGida@github.com>
2019-01-23 14:34:54 +0100

    added orderdao

---
+++ src/main/java/com/codecool/shop/dao/implementation/OrderDaoMem.java
@@ -0,0 +1,41 @@
+package com.codecool.shop.dao.implementation;
+
+import com.codecool.shop.dao.OrderDao;
+import com.codecool.shop.model.Order;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrderDaoMem implements OrderDao {
+    private List<Order> data = new ArrayList<>();
+    private static OrderDaoMem instance = null;
+
+    private OrderDaoMem() {
+    }
+
+    public static OrderDaoMem getInstance() {
+        if (instance == null) instance = new OrderDaoMem();
+        return instance;
+    }
+
+    @Override
+    public void add(Order order) {
+        order.setId(data.size() + 1);
+        data.add(order);
+    }
+
+    @Override
+    public Order find(int id) {
+        return data.stream().filter(t -> t.getId() == id).findFirst().orElse(null);
+    }
+
+    @Override
+    public void remove(int id) {
+        data.remove(find(id));
+    }
+
+    @Override
+    public List<Order> getAll() {
+        return data;
+    }
+}
