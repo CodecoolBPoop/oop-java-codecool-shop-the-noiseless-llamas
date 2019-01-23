8ff02befc279f8c48db92db0da7ed969473b85d9
Aron Gida <ifAronisGida@github.com>
2019-01-23 15:25:21 +0100

    orders now get saved serverside

--- src/main/java/com/codecool/shop/controller/CheckoutController.java
+++ src/main/java/com/codecool/shop/controller/CheckoutController.java
@@ -1,12 +1,15 @@
 package com.codecool.shop.controller;
 
 import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.dao.OrderDao;
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
 import com.codecool.shop.dao.ShoppingCartDao;
+import com.codecool.shop.dao.implementation.OrderDaoMem;
 import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
 import com.codecool.shop.dao.implementation.ShoppingCartDaoMem;
+import com.codecool.shop.model.Order;
 import com.codecool.shop.model.Product;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
@@ -17,23 +20,49 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 @WebServlet(urlPatterns = {"/checkout"})
 public class CheckoutController extends HttpServlet {
 
+    ProductDao productDataStore = ProductDaoMem.getInstance();
+    ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+    ShoppingCartDao shoppingCartsDataStore = ShoppingCartDaoMem.getInstance();
+    OrderDao orderDataStore = OrderDaoMem.getInstance();
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        ProductDao productDataStore = ProductDaoMem.getInstance();
-        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
-        ShoppingCartDao shoppingCartsDataStore = ShoppingCartDaoMem.getInstance();
+
 
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
         WebContext context = new WebContext(request, response, request.getServletContext());
-        context.setVariable("recipient", "World");
-        context.setVariable("category", productCategoryDataStore.find(1));
-        context.setVariable("products", productDataStore.getBy(productCategoryDataStore.find(1)));
         context.setVariable("cart", shoppingCartsDataStore.find(1));
         context.setVariable("currency", productDataStore.getAll().get(0).getDefaultCurrency());
         engine.process("checkout.html", context, response.getWriter());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HashMap<String, String> orderDetails = new HashMap<>();
+        int totalPrice = 0;
+        orderDetails.put("firstName", request.getParameter("firstName"));
+        orderDetails.put("lastName", request.getParameter("lastName"));
+        orderDetails.put("email", request.getParameter("email"));
+        orderDetails.put("city", request.getParameter("city"));
+        orderDetails.put("state", request.getParameter("state"));
+        orderDetails.put("zip", request.getParameter("zip"));
+        orderDetails.put("address", request.getParameter("address"));
+        Order newOrder = new Order(orderDetails, totalPrice, request.getParameter("paymentType"));
+        orderDataStore.add(newOrder);
+
+
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
+        WebContext context = new WebContext(request, response, request.getServletContext());
+        context.setVariable("cart", shoppingCartsDataStore.find(1));
+        context.setVariable("currency", productDataStore.getAll().get(0).getDefaultCurrency());
+
+        engine.process("checkout.html", context, response.getWriter());
+
     }
 }
--- src/main/java/com/codecool/shop/model/Order.java
+++ src/main/java/com/codecool/shop/model/Order.java
@@ -1,18 +1,18 @@
 package com.codecool.shop.model;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Order extends BaseModel {
 
     private ArrayList<Product> orderedItems;
-    private ArrayList<String> orderDetails;
+    private HashMap<String, String> orderDetails;
     private String paymentType;
     private float totalPrice;
-    private static int counter = 0;
     private boolean paid;
 
-    public Order(String name, ArrayList<String> orderDetails, float totalPrice, String paymentType) {
-        super(name);
+    public Order(HashMap<String, String> orderDetails, float totalPrice, String paymentType) {
+        super();
         this.paymentType = paymentType;
         this.orderDetails = orderDetails;
         this.totalPrice = totalPrice;
