9f7b26998e0ccc5d993db0bd275134893187ae34
Aron Gida <ifAronisGida@github.com>
2019-01-23 14:39:58 +0100

    added payment type field to Order

--- src/main/java/com/codecool/shop/model/Order.java
+++ src/main/java/com/codecool/shop/model/Order.java
@@ -6,12 +6,14 @@ public class Order extends BaseModel {
 
     private ArrayList<Product> orderedItems;
     private ArrayList<String> orderDetails;
+    private String paymentType;
     private float totalPrice;
     private static int counter = 0;
     private boolean paid;
 
-    public Order(String name, ArrayList<String> orderDetails, float totalPrice) {
+    public Order(String name, ArrayList<String> orderDetails, float totalPrice, String paymentType) {
         super(name);
+        this.paymentType = paymentType;
         this.orderDetails = orderDetails;
         this.totalPrice = totalPrice;
         this.paid = false;
