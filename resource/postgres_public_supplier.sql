66f88973ff6de88fb66f084aea620639f0c9e249
Doram13 <1991doram@gmail.com>
2019-02-05 14:12:29 +0100

    Database uploaded with products2

---
+++ resource/postgres_public_supplier.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.supplier (name, description) VALUES ('Amazon', 'Digital content and services');
+INSERT INTO public.supplier (name, description) VALUES ('Lenovo', 'Computers');
+INSERT INTO public.supplier (name, description) VALUES ('Mentors', 'Mentors rule');
