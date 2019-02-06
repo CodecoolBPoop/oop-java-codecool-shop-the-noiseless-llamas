8378f547f764ca1031a08ace94f2894a364049d4
istvanzsolnai <zsolnai.istvan1995@gmail.com>
2019-02-06 14:49:50 +0100

    Email sending

--- src/main/java/com/codecool/shop/controller/RegistrationController.java
+++ src/main/java/com/codecool/shop/controller/RegistrationController.java
@@ -4,7 +4,7 @@ package com.codecool.shop.controller;
 import com.codecool.shop.config.TemplateEngineUtil;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
-
+import com.codecool.shop.controller.GoogleMail;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,7 +14,7 @@ import java.io.IOException;
 
 @WebServlet(urlPatterns = {"/register"})
 public class RegistrationController extends HttpServlet {
-
+    String EMAIL;
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -24,8 +24,10 @@ public class RegistrationController extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        GoogleMail sendEmail = new GoogleMail();
 
         String emailAddress = request.getParameter("email");
+        EMAIL = emailAddress;
         String password = request.getParameter("password1");
         String passwordRepeat = request.getParameter("password2");
 
@@ -33,6 +35,13 @@ public class RegistrationController extends HttpServlet {
         System.out.println(password + " anyád");
         System.out.println(passwordRepeat + " anyád");
 
+
+    if (password == passwordRepeat){
+        sendEmail.main();
+        }
+       /* if (password == passwordRepeat)      {
+            sendEmail.sendFromGMail("codecoolshoptw5", "", emailAddressList, "CodecoolShop welcome message", "Welcome to Codecoolshop mofoo");
+        }*/
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
         WebContext context = new WebContext(request, response, request.getServletContext());
         engine.process("registration.html", context, response.getWriter());
